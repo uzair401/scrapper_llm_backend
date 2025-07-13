d1baa1ffff0d91714f05e774ec4985934e5b29fd
uzair401 <smartuzairsahil@gmail.com>
2025-07-13 17:43:34 +0500

    Post API Implementation

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 # urls.py
 
 from django.urls import path
-from .views import LinkedInAuthCheckView, LLMAskView
+from .views import LinkedInAuthCheckView, LLMAskView, LinkedInPostView
 
 urlpatterns = [
     path('api/linkedin/check-auth/', LinkedInAuthCheckView.as_view()),
     path('api/ask', LLMAskView.as_view(), name='llm-ask'),
+    path('api/linkedin/post', LinkedInPostView.as_view(), "Post-API")
 ]
