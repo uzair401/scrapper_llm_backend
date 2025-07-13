411f8fe05ef4b74a1fd6e2485a1c6c1541a4990c
uzair401 <smartuzairsahil@gmail.com>
2025-07-13 17:52:03 +0500

    Urls Update

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,5 +6,5 @@ from .views import LinkedInAuthCheckView, LLMAskView, LinkedInPostView
 urlpatterns = [
     path('api/linkedin/check-auth/', LinkedInAuthCheckView.as_view()),
     path('api/ask', LLMAskView.as_view(), name='llm-ask'),
-    path('api/linkedin/post', LinkedInPostView.as_view(), "Post-API")
+    path('api/linkedin/post/', LinkedInPostView.as_view(), name='linkedin-post'),
 ]
